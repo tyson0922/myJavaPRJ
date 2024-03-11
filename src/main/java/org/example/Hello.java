06c3dd7b4e0104dba8cfa9fad907ffa126a776f3
tyson0922 <taeseung92l@gmail.com>
2024-03-11 15:07:22 +0900

    code commit and push

--- src/main/java/org/example/Hello.java
+++ src/main/java/org/example/Hello.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Hello {
     public static void main(String[] args){
-        System.out.println("org.example.Hello!! My first Java coding!!");
+        System.out.println("Hello!! My first Java coding!!");
+        System.out.println("Edit Java Code!!");
     }
 }
