71187d4542ac18161a6b3b7afa601458ac73db03
tyson0922 <taeseung92l@gmail.com>
2024-03-14 12:45:25 +0900

    소스 업로드

---
+++ src/main/java/practice/Blackjack.java
@@ -0,0 +1,39 @@
+package practice;
+
+import java.util.Scanner;
+
+public class Blackjack {
+    public static void main(String[] args){
+        //ask usernumber1
+        int usernumber;
+                usernumber=21;
+                        System.out.println("Type in a number between 0 to 13");
+                        if (usernumber == 21){
+                            Scanner in = new Scanner(System.in);
+                            x =
+                            System.out.println("You win");
+
+                        }
+                        else if (usernumber < 21){
+                            System.out.println("Type in a number between 0 to 13");
+                        }
+                        else {
+
+
+                            System.out.println("you lose");
+        }
+
+
+        //save user number1
+        //dealer number1 generating and display
+        //dealer must stand if number is equal to or higher than 17
+        //dealer must continue if number lower than 17
+        //generate random number1
+        // add random number1 to usernumber1
+        //check if usernumber1 is over 21. if not than ask again to type in usernumber2 or no number
+        //add usernumber2 then check if over 21. if not than ask again
+        //if over 21 game over
+        //if 21 than win
+
+    }
+}
