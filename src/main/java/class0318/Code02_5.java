6465c35797de7832fe6b6373c398d45dc7c31500
tyson0922 <taeseung92l@gmail.com>
2024-03-18 13:36:45 +0900

    소스 업로드

---
+++ src/main/java/class0318/Code02_5.java
@@ -0,0 +1,10 @@
+package class0318;
+
+public class Code02_5 {
+    public static void main(String[] args){
+        int a = 100;
+        int b = 200;
+        int c = a+ b;
+        System.out.println(a + " + " + b + " = " +  c);
+    }
+}
