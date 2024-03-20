857015eb031af22a83362f70c255e6c2681c5d6b
tyson0922 <taeseung92l@gmail.com>
2024-03-20 18:53:08 +0900

    소스 업로드

--- src/main/java/class0320/TriangleWithStars.java
+++ src/main/java/class0320/TriangleWithStars.java
@@ -2,19 +2,20 @@ package class0320;
 
 public class TriangleWithStars {
 
-    public static void main(String[] args){
-        int i;
-        String str = "*";
-        for (i = 0; i < 5; i++){
-            if (i < 4) {
-                System.out.println(str);
-                str = str + "*";
+    public static void main(String[] args) {
+        int i, j;
+        for (i = 0; i <= 7; i++) {
+            if (i <= 3) {
+                for (j = 0; j <= i; j++)
+                    System.out.print("*");
+            } else {
+                for (j = 0; j <= 6 - i; j++) {
+                    System.out.print("*");
+
+                }
             }
-            else {
-                System.out.println("***");
-                System.out.println("**");
-                System.out.println("*");
-            }
+            System.out.println();
         }
+
     }
 }
