b55bb7380cdf1aa94f5972eec805af0e215219b9
tyson0922 <taeseung92l@gmail.com>
2024-03-21 18:52:06 +0900

    소스 업로드

---
+++ src/main/java/class0321/Code03_01.java
@@ -0,0 +1,11 @@
+package class0321;
+
+public class Code03_01 {
+    public static void main(String[] args){
+        int n1, n2, res;
+        n1 = 5;
+        n2 = 3;
+        res = n1 + n2;
+        System.out.println(res);
+    }
+}
---
+++ src/main/java/class0321/Code03_02.java
@@ -0,0 +1,20 @@
+package class0321;
+
+public class Code03_02 {
+    public static void main(String[] args){
+        int n1, n2, res;
+        n1 = 5;
+        n2 = 3;
+        res = n1 + n2;
+        System.out.println(res);
+        res = n1 - n2;
+        System.out.println(res);
+        res = n1 * n2;
+        System.out.println(res);
+        res = n1 / n2;
+        System.out.println(res);
+        res = n1 % n2;
+        System.out.println(res);
+
+    }
+}
