b44303970426dacd8d1e4c6ebc440370ed42dd3c
tyson0922 <taeseung92l@gmail.com>
2024-03-21 19:12:04 +0900

    소스 업로드

---
+++ src/main/java/class0321/Lab03_01.java
@@ -0,0 +1,19 @@
+package class0321;
+
+import java.util.Scanner;
+public class Lab03_01 {
+    public static void main(String[] args) {
+        double pound, kg;
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("파운드(lb)를 입력하세요: ");
+        pound = s.nextDouble();
+        kg = pound * 0.453592;
+        System.out.println(pound + "파운드(lb)는 " + kg + "킬로그램(kg)입니다.");
+
+        System.out.println("킬로그램(kg)을 입력하세요: ");
+        kg = s.nextDouble();
+        pound = kg / 0.4533592;
+        System.out.println(kg + "킬로그램(kg)은 " + pound + "파운드(lb)입니다.");
+    }
+}
