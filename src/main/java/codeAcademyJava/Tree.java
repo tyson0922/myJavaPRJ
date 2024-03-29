ea389177fd684dd1fde718c5a273023e6fdc636f
tyson0922 <taeseung92l@gmail.com>
2024-03-29 16:07:34 +0900

    소스 업로드

---
+++ src/main/java/codeAcademyJava/Tree.java
@@ -0,0 +1,24 @@
+package codeAcademyJava;
+
+public class Tree {
+    public static void main(String[] args){
+    /*
+    This code is going to print a tree on the screen.
+    */
+        System.out.println("Hey there, I'm Ariel and I'm learning to code in Java!");
+        System.out.println("I'm going to plant a tree today!");
+        System.out.println("  *  ");
+        System.out.println(" *** ");
+        System.out.println(" *** ");
+        System.out.println("  *  ");
+        System.out.println("  *  ");
+        System.out.println();
+        System.out.println(" * ** * ");
+        System.out.println(" ****** ");
+        System.out.println("  ****  ");
+        System.out.println("   **   ");
+        System.out.println("   **   ");
+        System.out.println("########");
+    }
+
+}
