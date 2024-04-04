4f326adbbfe874ce6a92b2a8f1c3195bf84e7cd1
tyson0922 <taeseung92l@gmail.com>
2024-04-04 17:19:57 +0900

    소스 업로드

--- src/main/java/class0401/Quiz1.java
+++ src/main/java/class0401/Quiz1.java
@@ -8,12 +8,12 @@ public class Quiz1 {
         int a;
         System.out.println("자연수를 입력하세요: ");
         a = s.nextInt();
-        if (a%15 == 0){
-            System.out.println("3과 5의 배수입니다.");
+        if (a%21 == 0){
+            System.out.println("3과 7의 배수입니다.");
         } else if (a%3 == 0) {
             System.out.println("3의 배수입니다.");
-        } else if (a%5 == 0){
-            System.out.println("5의 배수입니다.");
+        } else if (a%7 == 0){
+            System.out.println("7의 배수입니다.");
         } else {
             System.out.println("조건에 만족하지 않습니다.");
         }
