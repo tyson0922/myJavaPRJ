2ba96d5a9c1952a74893f7925c0ea56d91041077
tyson0922 <taeseung92l@gmail.com>
2024-04-05 19:14:39 +0900

    소스 업로드

--- src/main/java/codeAcademyJava/Practice/practice0404/Store.java
+++ src/main/java/codeAcademyJava/Practice/practice0404/Store.java
@@ -17,5 +17,9 @@ public class Store {
     // main method
     public static void main(String[] args) {
         Store cookieShop = new Store("cookies", 12, 3.75);
+        System.out.println(cookieShop.productType);
+        System.out.println(cookieShop.inventoryCount);
+        System.out.println(cookieShop.inventoryPrice);
+
     }
 }
