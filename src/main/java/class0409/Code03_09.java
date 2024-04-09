59078cf65a79dadc3685dca0d11d8554f02de27a
tyson0922 <taeseung92l@gmail.com>
2024-04-09 14:00:46 +0900

    소스 업로드

---
+++ src/main/java/class0409/Code03_09.java
@@ -0,0 +1,16 @@
+package class0409;
+
+public class Code03_09 {
+    public static void main(String[] args) {
+        int num = 20;
+        ++num; System.out.println(++num + " : num 변수 출력 전에 +1이 되어서 21이 출력됨");
+        num++; System.out.println(num++ + " : nun 변수가 출력되고 +1 되기에 21이 출력됨");
+        num--; System.out.println(num + "");
+        num += 3; System.out.println(num + "");
+        num -= 3; System.out.println(num + "");
+        num *= 3; System.out.println(num + "");
+        num /= 3; System.out.println(num + "");
+        num %= 3; System.out.println(num + "");
+
+    }
+}
