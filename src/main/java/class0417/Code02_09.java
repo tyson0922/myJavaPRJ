aa8a2df431ee1db3aff7393d63460c51368075f6
tyson0922 <taeseung92l@gmail.com>
2024-04-17 10:58:02 +0900

    소스 업로드

---
+++ src/main/java/class0417/Code02_09.java
@@ -0,0 +1,17 @@
+package class0417;
+
+public class Code02_09 {
+    public static void main(String[] args){
+        int num1 = 10, num2 = 20;
+        int resAdd;
+        int resSub;
+        int resMul;
+        double resDiv;
+        resAdd = num1 + num2;
+        resSub = num1 - num2;
+        resMul = num1 * num2;
+        resDiv = (double)num1 / num2;
+        System.out.println("results are" + "\n " + resAdd +"\n"+ resSub + "\n"+resMul+"\n"+ resDiv);
+
+    }
+}
---
+++ src/main/java/classPractice0325/Quiz1.java
@@ -0,0 +1,11 @@
+package classPractice0325;
+
+public class Quiz1 {
+    public static void main(String[] args) {
+        int a=3, b=4 , c=5;
+        int d = b + c;
+
+        System.out.println(a + "+" + b + c);
+        System.out.println((a + d) + "+" + (b + c));
+    }
+}
