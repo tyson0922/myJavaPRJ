7a99c7ef5beb2aa306bff1b705137469fc6260bc
tyson0922 <taeseung92l@gmail.com>
2024-04-18 15:26:07 +0900

    소스 업로드

--- src/main/java/class0326/TriangleWithStar2.java
+++ src/main/java/class0326/TriangleWithStar2.java
@@ -7,6 +7,12 @@ public class TriangleWithStar2 {
         b = 0;
         c = 0;
         d = 0;
+        /*
+        0001000     네번째 자리에
+        0011100
+        0111110
+        1111111
+         */
         for (d = 0; d < 4; d++) {
             for (a = 1; a < 8; a++) {
                 b = 8 - a;
