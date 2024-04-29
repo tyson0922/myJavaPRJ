43112719e05a93188de1cd8f6c89642bd3b20eea
tyson0922 <taeseung92l@gmail.com>
2024-04-29 18:12:19 +0900

    소스 업로드

---
+++ src/main/java/programmers/PythagoreanTheorem.java
@@ -0,0 +1,13 @@
+package programmers;
+import java.util.Scanner;
+public class PythagoreanTheorem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int c = sc.nextInt();
+
+        int b_square = c*c - a*a;
+
+        System.out.println(b_square);
+    }
+}
