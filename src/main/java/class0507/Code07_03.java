382f95535c13784a9a6f42f602602328ca1ceb62
tyson0922 <taeseung92l@gmail.com>
2024-05-07 14:56:06 +0900

    소스 업로드

---
+++ src/main/java/class0507/Code07_03.java
@@ -0,0 +1,12 @@
+package class0507;
+
+import sample.Turtle;
+
+public class Code07_03 {
+
+    public static void main(String[] args) {
+//        Turtle turtle = new Turtle();
+
+        Turtle turtle1 = new Turtle();
+    }
+}
