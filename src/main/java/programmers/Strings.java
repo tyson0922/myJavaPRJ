7a48d93d380798b4d98fec04c9edbe230d9f12ba
tyson0922 <taeseung92l@gmail.com>
2024-05-10 10:25:21 +0900

    소스 업로드

---
+++ src/main/java/programmers/Strings.java
@@ -0,0 +1,10 @@
+package programmers;
+import java.util.Scanner;
+
+public class Strings {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        System.out.println(a);
+    }
+}
