2dfa72210cf3db4f2097c5a1ef8f70ba6f533caa
tyson0922 <taeseung92l@gmail.com>
2024-08-16 12:36:34 +0900

    소스 업로드

---
+++ src/main/java/practice/Code12_08.java
@@ -0,0 +1,24 @@
+package practice;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Code12_08 {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("목록 선택");
+        frame.setLayout(new FlowLayout());
+
+        String[] petAry = {"고양이", "강아지", "토끼", "코알라", "미어켓"};
+
+        JList list = new JList(petAry);
+        frame.add(list);
+
+        JComboBox combo = new JComboBox(petAry);
+        frame.add(combo);
+
+        frame.setSize(250, 250);
+        frame.setVisible(true);
+    }
+}
